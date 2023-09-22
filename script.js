aa643ed404118a6a9a273f7ba9a3a46aae9d93f6
Jared Byrum <jaredbyrum@gmail.com>
2023-09-22 02:21:40 -0400

    finsihed base HTMLand CSS styling, connected google fonts and weather API

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 //timer
 var today = dayjs()
 $('#time').text(dayjs().format("MMMM DD, YYYY"));
+//lat and lon variables
+var lat = '';
+var lon = '';
 $(function(){
+    function getForecast(){
+        var requestURL = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=13b4e423f9a07e67b579be6abba16366";
+        
+        fetch(requestUrl)
+            .then(function (response) {
+            return response.json();
+         })
+            .then(function(){
 
+            })
+
+    }
 
 
 
