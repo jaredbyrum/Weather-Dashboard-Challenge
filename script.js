e88edc5109c6a158800ccb7918d4faec5894bf6b
Jared Byrum <jaredbyrum@gmail.com>
2023-09-25 22:13:53 -0400

    api calls work correctly, adding func to print to cards.

--- script.js
+++ script.js
@@ -9,16 +9,54 @@
 //save responses to different elements
 //save serach to local storage and diplay on LEFT UL make buttons that refresh that cities fetch
 // api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=13b4e423f9a07e67b579be6abba16366"
-
+// 0, 8, 16, 24, 32, 39 list items
 
 $(function(){
     var today = dayjs()
     $('#time').text(dayjs().format("MMMM DD, YYYY"));
     
    
-    //geocode api fetch
-    function geo(){
+    var cityName = '';
+    //generate elements func
+    function loadList(){
+        $('#prev-search').eq(9).remove();
+        $("<button id='history' class='btn btn-secondary w-100 my-3'>"+cityName+"</button>").prependTo('#prev-search');
+    }
+    //only save ten items func
+    var i = 0;
+    function saveItems(){
+        if (i === 10){
+            i = 0;
+        } else {
+            i++; 
+            localStorage.setItem(i, cityName);  
+        } 
+    }
+
+    //func for main button
+    $('#submit').click(function(event){
+        event.preventDefault();
+        cityName = $('#city-search').val();
+        loadList();
+        saveItems();
+        weather(cityName);
+    })
+
+    //func for search history
+    $('#history').on('click', function(event){
+        event.preventDefault();
+        cityName = event.target.innerHTML;
+        loadList();
+        saveItems();
+        weather(cityName);
+    })
+    
+    
+
+    //weather function
+    function weather(cityName){
         var geocodeAPI = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + ",US&appid=13b4e423f9a07e67b579be6abba16366";
+        
         fetch(geocodeAPI)
             .then(function (response){
                 return response.json();
@@ -28,62 +66,33 @@ $(function(){
                 //finds the city in the returned array that best matches cityName
                 for(let i = 0; i < data.length; i++){
                     if (data[i].name === cityName){
-                        var lat = data[i].lat
-                        var lon = data[i].lon
+                        console.log('here', data[i])
+                        var lat = data[i].lat.toString()
+                        var lon = data[i].lon.toString()
                     }
                 }
-            })
-    }
-
-    function weather(){
-        var latStr = lat.toString();
-        var lonStr = lon.toString();
-        var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latStr + "&lon=" + lonStr + "&appid=13b4e423f9a07e67b579be6abba16366";
-         fetch(weatherAPI)
-            console.log(weatherAPI)
-            .then(function(response){
-                return response.json();
-            })
-            .then(function(data){
-                console.log(data)
-            })
-    }  
-    var cityName = '';
-    var i;
-    i = 0;
-    //generate elements func
-    function loadList(){
-        $('#prev-search').eq(9).remove();
-        $("<button class='btn btn-secondary w-100 my-3'>"+cityName+"<button>").prependTo('#prev-search');
-    }
-    //only save ten items func
-
-
-
-
-
-
-    //func for main button
-    $('#submit').click(function(event){
-        event.preventDefault();
-        cityName = $('#city-search').val();
-        i++;
-        localStorage.setItem(i, cityName)
-        loadList(cityname)
-    })
-
-    //func for search history
-    $('#history').on('click', function(event){
-        event.preventDefault();
-        cityName = event.target.innerHTML;
-        i++;
-        localStorage.setItem(i, cityName)
-        loadList(cityName)
-    })
-    
-       
+        var weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=13b4e423f9a07e67b579be6abba16366&units=imperial";        
+            fetch(weatherAPI)
+                //
+                .then(function(response){
+                    return response.json();
+                })
+                .then(function(data){
+                    console.log(data)
+                    
+                })    
+            })   
+            
+    }   
         
-        
+    function printWeather(cityName, ){
+        var weather = {
+            temp: data.list[i].main.temp,
+            humidity: data.list[i].main.humidity,
+            wind: data.list[i].wind.speed,
+            icon: data.list[i].weather.icon
+        }
+    }    
         
         
         
